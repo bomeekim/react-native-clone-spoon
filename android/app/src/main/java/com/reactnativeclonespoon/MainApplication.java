3d0f1f27bf14d6422c8e780c5e4ece17fdc06e12
bomeekim <thespringg@naver.com>
2021-07-27 16:31:43 +0900

    Android, iOS package 및 파일 이름 변경

--- android/app/src/main/java/com/reactnativeclonespoon/MainApplication.java
+++ android/app/src/main/java/com/reactnativeclonespoon/MainApplication.java
@@ -1,4 +1,4 @@
-package com.floraspooncastapp;
+package com.reactnativeclonespoon;
 
 import android.app.Application;
 import android.content.Context;
@@ -64,7 +64,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.floraspooncastapp.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.reactnativeclonespoon.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
